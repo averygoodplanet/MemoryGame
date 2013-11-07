db1f845bd7475d98ddfe838455d5873b46a714a8
Peter Himmelreich <averygoodplanet@gmail.com>
2013-11-07 14:11:57 -0600

    createGameboard array returns a proper shuffled array.

--- 2013-11-07-Memory/routes/game.js
+++ 2013-11-07-Memory/routes/game.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var Game = mongoose.model('Game');
+var _ = require('lodash');
 
 // var colors = require('colors');
 // Colors
@@ -17,8 +18,29 @@ exports.index = function(req, res){
 
 // POST /new
 exports.create = function(req, res){
+  // create array
+  console.log("req.body.numPairs");
+  console.log(req.body.numPairs);
+  var gameboardArray = createGameBoardArray(req.body.numPairs);
+  //  req.body.gameBoard = array.
+
+  // new Game(newData);
   new Game(req.body).save(function(err, game){
     console.log('game: ' +game);
     res.send(game);
+    //check that we are returning a proper array
+    //in gameBoard property in browser.
   });
 };
+
+function createGameBoardArray(numPairs){
+  var array = [];
+  var intPairs = parseInt(numPairs);
+  var array2 = (_.range(1, (intPairs+1)));
+  var array3 = (_.range(1, (intPairs+1)));
+  array = array2.concat(array3);
+  array = _.shuffle(array);
+  console.log('array: ');
+  console.log(array);
+  return array;
+}
