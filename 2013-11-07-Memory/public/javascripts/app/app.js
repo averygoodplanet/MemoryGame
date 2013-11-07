3d3df2d0fae00f1f55704d297aa1e8c21fac6a98
Jerry Sims <jerrysims07@gmail.com>
2013-11-07 16:43:06 -0600

    process click states

--- 2013-11-07-Memory/public/javascripts/app/app.js
+++ 2013-11-07-Memory/public/javascripts/app/app.js
@@ -37,12 +37,36 @@ function clickCard(e) {
   var $clickedCard = $(this);
   var cardLocation = $clickedCard.data('id');
   var gameid = $('#gameBoard').data('gameid');
+
+  // if card is already face up, exit function
+  if($clickedCard.hasClass('faceUp'))
+    return;
+
+  // check for number of active cards (0, 1, or 2)
+  switch($('.faceUp').length){
+    case 0:
+      console.log('zero card up');
+    break;
+
+    case 1:
+    console.log('one card up');
+    break;
+
+    case 2:
+    console.log('two cards up');
+    break;
+
+    default:
+    console.log('other');
+  }
+
+
   //send cardLocation via ajax to server and get the card's hidden value back
   sendGenericAjaxRequest('/click', {cardLocation: cardLocation, gameid: gameid }, 'GET', null, e, function(card, status, jqXHR){
     console.log(card);
-    htmlFlipCard($clickedCard, card.value);
-
+  htmlFlipCard($clickedCard, card.value);
   });
+
 }
 
 //----------------------------------------------------------------------------//
